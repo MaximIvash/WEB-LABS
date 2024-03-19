75a561213693c74ecb423f01b604f776b20f3e14
MaximIv <cradd860@gmail.com>
2024-03-19 18:34:42 +0200

    [lab 2][task 1 added]

--- LAB1/code/index.php
+++ LAB1/code/index.php
@@ -1,2 +1,12 @@
 <?php
-echo phpinfo();
+echo "<pre>";
+
+// Задание №1
+echo "<h3>№1</h3>";
+$very_bad_unclear_name = "15 chicken wings";
+
+$order = &$very_bad_unclear_name;
+$order .= ", with big cola";
+
+echo "\nYour order is: $very_bad_unclear_name.\n";
+
