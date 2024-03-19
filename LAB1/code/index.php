81765845088904d551be5a7f15595d8f8aa3e6d3
MaximIv <cradd860@gmail.com>
2024-03-19 18:49:59 +0200

    [lab 2][task 2 added]

--- LAB1/code/index.php
+++ LAB1/code/index.php
@@ -9,4 +9,21 @@ $order = &$very_bad_unclear_name;
 $order .= ", with big cola";
 
 echo "\nYour order is: $very_bad_unclear_name.\n";
+// Задание №2
+echo "<h3>№2</h3>";
+$int = 36;
+echo $int;
+echo "\n";
+
+$float = 6.17;
+echo $float;
+echo "\n";
+
+echo $int - 24;
+echo "\n";
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+echo ($last_month - $this_month);
+echo "\n";
 
